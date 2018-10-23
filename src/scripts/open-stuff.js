55913a1dbb73e4a748af0b36fbb2aba0a74fd09f
Andre Dantas <andre.dantas.lima@gmail.com>
2018-10-22 22:47:38 -0400

    Opening and closing modal with js

---
+++ src/scripts/open-stuff.js
@@ -0,0 +1,16 @@
+const body = document.body;
+
+// Open modal with space bar
+const modal = document.querySelector('.modal');
+body.addEventListener("keyup", (event) => {
+    if (event.key === 'o') {
+        body.classList.add('modal-is-open');
+        modal.classList.add('modal-is-open');
+    }
+});
+
+// Close modal when clicking the x button
+document.querySelector('.card-close-icon').addEventListener('click', () => {
+    body.classList.remove('modal-is-open');
+    modal.classList.remove('modal-is-open');
+});
